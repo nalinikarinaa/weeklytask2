a63e71059367a6c9928ca15e022b07f21774de24
Nalini Karina Andhini <nalinikarina613@gmail.com>
2024-09-25 15:22:14 +0700

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        "coklattua": '#1f1810',
+        "kuning": '#ffad31',
+    }
+    },
+  },
+  plugins: [],
+}
+
