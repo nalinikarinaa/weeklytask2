db3f8cb2516005884585aa008d24ff0be6efd455
Nalini Karina Andhini <nalinikarina613@gmail.com>
2024-09-26 13:43:15 +0700

    index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx,vue}', // File dalam folder src
+    './*.html', // Tambahkan jalur file HTML di luar folder src
+  ],
   theme: {
     extend: {
+      // fontFamily: {
+      //   sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+      //   serif: ['Merriweather', 'serif'],
+      // },
       colors : {
         "coklattua": '#1f1810',
         "kuning": '#ffad31',
@@ -11,4 +18,5 @@ module.exports = {
   },
   plugins: [],
 }
+
 
